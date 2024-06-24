e0ed01d405ba0967b271c96812cee0ebfa7306a1
Guy Liba <guy33liba@gmail.com>
2024-06-24 16:11:05 +0200

    gdfsa

---
+++ backend/routes/user.js
@@ -0,0 +1,9 @@
+import express from "express"
+import mongoose from "mongoose"
+
+const router = express.Router()
+
+router.post("/login", (req, res) => {})
+router.post("/signup", (req, res) => {})
+
+export default router
