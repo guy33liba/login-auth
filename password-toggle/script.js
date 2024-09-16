1c379b6f47d2c89496c1fedafa17190c7066bef5
Guy Liba <guy33liba@gmail.com>
2024-09-16 16:00:47 +0200

    hidden style

--- password-toggle/script.js
+++ password-toggle/script.js
@@ -1,2 +1,13 @@
 const name = document.querySelector("name")
 const password = document.querySelector("password")
+const eye = document.querySelector(".eye")
+const eyehide = document.querySelector(".eyehide")
+
+eye.onclick = () => {
+  eyehide.classList.remove("hidden")
+  eye.classList.add("hidden")
+}
+eyehide.onclick = () => {
+  eye.classList.remove("hidden")
+  eyehide.classList.add("hidden")
+}
