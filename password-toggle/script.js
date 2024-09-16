53c56878f6980696a831ae99e34e11ed62663c80
Guy Liba <guy33liba@gmail.com>
2024-09-16 16:13:42 +0200

    passwordtype

--- password-toggle/script.js
+++ password-toggle/script.js
@@ -1,13 +1,32 @@
 const name = document.querySelector("name")
-const password = document.querySelector("password")
+const password = document.querySelector(".password")
 const eye = document.querySelector(".eye")
 const eyehide = document.querySelector(".eyehide")
-
+password.value = "hello"
 eye.onclick = () => {
-  eyehide.classList.remove("hidden")
-  eye.classList.add("hidden")
+  eyehide.classList.remove("hidden") // Show eyehide
+  eye.classList.add("hidden") // Hide eye
+  if (eye.classList.contains("hidden")) {
+    password.type = "text"
+  }
 }
+
 eyehide.onclick = () => {
   eye.classList.remove("hidden")
   eyehide.classList.add("hidden")
+  if (!eye.classList.contains("hidden")) {
+    password.type = "password"
+  }
 }
+
+// eye.onclick = () => {
+//   eye.classList.add('hidden');
+//   eyehide.classList.remove('hidden');
+//   password.setAttribute("type", "text");  // Show password
+// }
+
+// eyehide.onclick = () => {
+//   eyehide.classList.add('hidden');
+//   eye.classList.remove('hidden');
+//   password.setAttribute("type", "password");  // Hide password
+// }
